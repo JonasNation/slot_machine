e048ffbd977d63bb31713895369f5015a8bd8521
JonasNation <laimnation@gmail.com>
2023-01-31 19:03:59 -0600

    created function that gets input from user to chose bet lines

--- slot.py
+++ slot.py
@@ -3,12 +3,17 @@
 # Then a win will be determined
 # the user can keep playing until they cash out or lose all their money
 
+MAX_LINES = 7
+MAX_BET = 100
+MIN_BET = 10
 
 # Collecting user input for money deposit
+
+
 def deposit():
     while True:
         amount = input("What is your deposit amount? $")
-        # checks input to make sure it is a valid number and nothing else
+        # checks input to make sure it is a valid number (whole) and nothing else
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -20,14 +25,32 @@ def deposit():
             print("Enter your amount")
     return amount
 
+# collecting bet from user
 
-deposit()
+
+def bet():
+    # gets number of line for slot machine
+    while True:
+        lines = input(
+            "Enter number of line to bet on (1 - {})? ".format(str(MAX_LINES)))
+        # checks input to make sure it is a valid number and nothing else
+        if lines.isdigit():
+            lines = int(lines)
+            if lines in range(1, MAX_LINES):
+                break
+            else:
+                print("Enter valid number of lines")
+        else:
+            print("Please enter your number")
+    return lines
 
 # main section
 
 
 def slots():
     balance = deposit()
+    lines = bet()
+    print(balance, lines)
 
 
 slots()
